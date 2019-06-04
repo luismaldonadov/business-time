5c89b8cb4b4c381f1de44920789eca182fdb349a
Luis Maldonado <maldonadovla@gmail.com>
2019-06-03 18:39:00 -0700

    Solution

--- businessTime.js
+++ businessTime.js
@@ -1,5 +1,11 @@
 
-
+/**
+ *
+ *
+ * @param {*} holiday object {start: <Date>, end: <Date>}
+ * @param {*} time Date Object
+ * @param {*} duration number of ms, positive or negative
+ */
 function addBusinessTime(holiday, time, duration) {
   
   // Returns a Date object
